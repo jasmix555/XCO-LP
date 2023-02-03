9834a46011349ba6333e1a70c96f3ff5c912e108
jasmix555 <94930720+jasmix555@users.noreply.github.com>
2023-02-03 09:17:10 +0900

    Contents of XCO LP

---
+++ js/app.js
@@ -0,0 +1,10 @@
+document.addEventListener("mousemove", parallax);
+function parallax(e) {
+    document.querySelectorAll('.imgP').forEach(function (move) {
+        const speed = move.getAttribute('data-speed');
+        const x = (window.innerWidth - e.pageX * speed) / 1000;
+        const y = (window.innerHeight - e.pageY * speed) / 500;
+        move.style.transform = `translateX(${x}px) translateY(${y}px)`;
+    });
+}
+
